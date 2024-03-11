a0a261bb7f0eb45e07c590029c33c6aafffceb2b
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-03-12 00:19:05 +0530

    feat: expose username

--- src/auth/lucia.service.ts
+++ src/auth/lucia.service.ts
@@ -24,6 +24,7 @@ export class LuciaService extends Lucia {
       getUserAttributes: (attributes) => {
         return {
           email: attributes.email,
+          username: attributes.username,
         }
       },
     })
