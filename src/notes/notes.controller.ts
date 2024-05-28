9bab856fdd0b629513db1c0b82249a4fc0205f0c
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-05-28 21:33:12 +0530

    feat: `/notes` endpoint
    - use auth guard at the `/notes` controller

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -6,27 +6,34 @@ import {
   Param,
   Post,
   UsePipes,
-  Res
+  Res,
+  UseGuards
 } from '@nestjs/common'
 import { NotesService } from './notes.service.js'
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe.js'
 import { CreateNotesDto, createNotesSchema } from './dto/notes-dto.js'
+import {AuthGuard} from "../auth/auth.guard.js"
 import { Response } from 'express'
 
+
 @Controller('notes')
+@UseGuards(AuthGuard)
 export class NotesController {
   constructor(private notesService: NotesService) {}
 
   @Get()
-  async getAllNotes() {
-    return this.notesService.getAllNotes()
+  async getAllNotes(@Res({passthrough: true}) res: Response) {
+    if(!res.locals.user) {
+      return res.status(401).send('Unauthorized')
+    }
+    return this.notesService.getNotesByUser(res.locals.user)
   }
 
-  @Get(':id')
-  async getNoteById(@Param() params: any) {
-    Logger.debug('GET_NOTE_BY_ID', params.id)
-    return this.notesService.getNoteById(params.id)
-  }
+  // @Get(':id')
+  // async getNoteById(@Param() params: any) {
+  //   Logger.debug('GET_NOTE_BY_ID', params.id)
+  //   return this.notesService.getNoteById(params.id)
+  // }
 
   @Post()
   @UsePipes(new ZodValidationPipe(createNotesSchema))
